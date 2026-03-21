94b27e89ddf7baac0c9e47c12dbae5af43b37a58
Thiranjaya Siriwardhana <kanujathiranjaya@gmail.com>
2026-03-21 16:58:24 +0530

    feat: Add temporary C# project to generate BCrypt password hashes.

---
+++ tmp/tmp_gen_pwd/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using BCrypt.Net;
+
+class Program
+{
+    static void Main()
+    {
+        string[] users = { "Storekeeper", "DivHead", "Employee", "Super", "Accountant" };
+        string[] passwords = {
+            "StorekeeperPass123!",
+            "DivHeadPass123!",
+            "EmployeePass123!",
+            "SuperintendentPass123!",
+            "AccountantPass123!"
+        };
+
+        for (int i = 0; i < users.Length; i++)
+        {
+            string hash = BCrypt.Net.BCrypt.HashPassword(passwords[i]);
+            Console.WriteLine($"{users[i]}|{passwords[i]}|{hash}");
+        }
+    }
+}
