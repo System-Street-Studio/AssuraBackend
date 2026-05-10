606229431cc17398796f9eb133a7f7a7f572d68c
Saranga <sarangamadhubhashani2001@gmail.com>
2026-05-11 00:13:57 +0530

    feat: add migrations and database updated

---
+++ src/Assura.Infrastructure/Migrations/20260508161500_updatetransferNullubleValue.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Assura.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatetransferNullubleValue : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transfers_Users_TargetUserId",
+                table: "Transfers");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TargetUserId",
+                table: "Transfers",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transfers_Users_TargetUserId",
+                table: "Transfers",
+                column: "TargetUserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transfers_Users_TargetUserId",
+                table: "Transfers");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TargetUserId",
+                table: "Transfers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transfers_Users_TargetUserId",
+                table: "Transfers",
+                column: "TargetUserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
