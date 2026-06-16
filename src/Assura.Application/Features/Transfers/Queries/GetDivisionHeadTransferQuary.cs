6983bb78d71a050a2e9684262650bfb9fa7c30ee
Thiranjaya Siriwardhana <kanujathiranjaya@gmail.com>
2026-06-16 17:33:14 +0530

    feat: add query handler to retrieve division-specific asset transfers based on status tabs

--- src/Assura.Application/Features/Transfers/Queries/GetDivisionHeadTransferQuary.cs
+++ src/Assura.Application/Features/Transfers/Queries/GetDivisionHeadTransferQuary.cs
@@ -45,7 +45,7 @@ public class GetDivisionHeadTransferQueryHandler : IRequestHandler<GetDivisionHe
         query = request.Tab.ToLower() switch
         {
             "outgoing" => query.Where(t => t.ToDivisionId == headDivisionId 
-                                        && t.Status == TransferStatus.PendingOwnerApproval || t.Status == TransferStatus.Cancelled),
+                                        && (t.Status == TransferStatus.PendingOwnerApproval || t.Status == TransferStatus.Cancelled)),
 
             "incoming" => query.Where(t => t.FromDivisionId == headDivisionId 
                                         && t.Status == TransferStatus.PendingOwnerDivisionHeadApproval),
